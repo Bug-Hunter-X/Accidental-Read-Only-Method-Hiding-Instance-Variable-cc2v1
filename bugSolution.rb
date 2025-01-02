0eb1d63b7b0e5d4e6ddf9caf13c2001bc5952736
BugHunter-X <askzenai@gmail.com>
2025-01-02 09:22:05 +0100

    Add bugSolution.rb file

---
+++ bugSolution.rb
@@ -0,0 +1,14 @@
+```ruby
+class MyClass
+  attr_accessor :value # Use attr_accessor for read-write access
+  
+  def initialize(value)
+    @value = value
+  end
+end
+
+my_object = MyClass.new(10)
+puts my_object.value # Output: 10
+my_object.value = 20
+puts my_object.value # Output: 20
+```
